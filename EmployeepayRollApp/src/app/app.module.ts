7af70a23840a7f87906d81b0fa7641167a8ef6c0
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-08-26 11:12:52 +0530

    make functionality of home page

---
+++ EmployeepayRollApp/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { AddComponent } from './component/add/add.component';
+import { EmployeeDetailsComponent } from './component/employee-details/employee-details.component';
+import { HomeComponent } from './component/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AddComponent,
+    EmployeeDetailsComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ EmployeepayRollApp/src/app/component/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/model/employee';
+import { HttpService } from 'src/app/service/http.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  public employeeCount: number = 10;
+  public employeeDetails: Employee[] = [];
+
+  constructor(private httpService: HttpService) { }
+
+  ngOnInit(): void {
+    this.httpService.getEmployeeData().subscribe(data=>{
+      this.employeeDetails = data;
+      this.employeeCount = this.employeeDetails.length;
+      console.log(this.employeeDetails);
+    });
+    //console.log(this.httpService.getEmployeeData);
+  }
+}
